96e7a97477f34d1614def4340c75c1b42ea1482b
Vendula Krajíčková <vendysacek@seznam.cz>
2023-01-20 17:23:04 +0100

    Hotový úkol

---
+++ main.js
@@ -0,0 +1,27 @@
+// tady je místo pro náš program
+
+// změna barvy písma nadpisu
+let barva = document.querySelector('.nadpis');
+barva.style.color = '#13529e';
+
+// změna velikosti písma odstavce
+let velikost = document.querySelector('.odstavec');
+velikost.style.fontSize = '20px';
+
+//změna stylu odstavce
+let styl = document.querySelector('.odstavec');
+styl.style.fontStyle = 'italic';
+
+//změna obrázku
+let zvire = document.querySelector('.obrazek');
+zvire.src = 'kocka.jpg';
+
+//posunutí tvaru
+let posun = document.querySelector('.ctverecek');
+posun.style.marginLeft = '100px';
+
+//otočení tvaru
+let otoc = document.querySelector('.ctverecek');
+otoc.style.transform = 'rotate(-15deg)';
+
+
