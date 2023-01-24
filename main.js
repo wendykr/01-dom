68ba2132995b8f636ec5557b7e1de3fe240741a0
Vendula Krajíčková <vendysacek@seznam.cz>
2023-01-24 18:32:52 +0100

    Úprava zápisu více vlastností najednou

--- main.js
+++ main.js
@@ -4,24 +4,18 @@
 let barva = document.querySelector('.nadpis');
 barva.style.color = '#13529e';
 
-// změna velikosti písma odstavce
-let velikost = document.querySelector('.odstavec');
-velikost.style.fontSize = '20px';
-
-//změna stylu odstavce
-let styl = document.querySelector('.odstavec');
-styl.style.fontStyle = 'italic';
+// změna velikosti písma a stylu odstavce
+let formatovani = document.querySelector('.odstavec');
+formatovani.style.fontSize = '20px';
+formatovani.style.fontStyle = 'italic';
 
 //změna obrázku
 let zvire = document.querySelector('.obrazek');
 zvire.src = 'kocka.jpg';
 
-//posunutí tvaru
-let posun = document.querySelector('.ctverecek');
-posun.style.marginLeft = '100px';
-
-//otočení tvaru
-let otoc = document.querySelector('.ctverecek');
-otoc.style.transform = 'rotate(-15deg)';
+//posunutí a otočení tvaru
+let transformace = document.querySelector('.ctverecek');
+transformace.style.marginLeft = '100px';
+transformace.style.transform = 'rotate(-15deg)';
 
 
